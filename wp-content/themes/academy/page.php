43cc6a3c80972cc2dc7c159f8fd4777af0b873c1
Len <len.zeecode@gmail.com>
2019-01-02 18:17:43 +0700

    update single page

--- wp-content/themes/academy/page.php
+++ wp-content/themes/academy/page.php
@@ -32,50 +32,7 @@
 								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
 							</ul>
 						</div> -->
-						<div class="comment-main">
-							<div class="comment-title">
-								<h2>Leave a Reply</h2>
-								<p>Your email address will not be published. Required fields are marked *</p>
-							</div>
-							<!-- Form -->
-							<form class="form" action="#">
-								<div class="row">
-									<div class="col-lg-6 col-12">
-										<div class="form-group">
-											<input name="name" type="text" placeholder="Full name">
-										</div>
-									</div>
-									<div class="col-lg-6 col-12">
-										<div class="form-group">
-											<input name="email" type="email" placeholder="Enter your email">
-										</div>
-									</div>
-									<div class="col-lg-6 col-12">
-										<div class="form-group">
-											<input name="subject" type="text" placeholder="Subjects">
-										</div>
-									</div>
-									<div class="col-lg-6 col-12">
-										<div class="form-group">
-											<input name="address" type="url" placeholder="Web Address">
-										</div>
-									</div>
-									<div class="col-12">
-										<div class="form-group">
-											<textarea name="address" rows="6" placeholder="Web Address"></textarea>
-										</div>
-									</div>
-									<div class="col-12">
-										<div class="form-group">
-											<div class="button">
-												<button type="submit" class="btn primary">Post Comment</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</form>
-							<!--/ End Form -->
-						</div>
+						
 					</div>
 				</div>
 				<?php endwhile; ?>

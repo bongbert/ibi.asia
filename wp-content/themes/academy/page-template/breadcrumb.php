8efefa3593760d6c16dbe8949ed10a919d5f5799
Len <len.zeecode@gmail.com>
2019-01-19 12:22:11 +0700

    update header

--- wp-content/themes/academy/page-template/breadcrumb.php
+++ wp-content/themes/academy/page-template/breadcrumb.php
@@ -5,7 +5,7 @@
 				<div class="col-12">
 					<ul class="bread-list">
 						<li><a href="<?= home_url(); ?>">Home<i class="fa fa-angle-right"></i></a></li>
-						<li class="active"><a href="blog-left-sidebar.html"><?php the_title(); ?></a></li>
+						<li class="active"><a href="#"><?php the_title(); ?></a></li>
 					</ul>
 				</div>
 			</div>

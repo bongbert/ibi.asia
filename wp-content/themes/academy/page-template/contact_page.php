3f17ba3542470ced4617acbd9e0ba175547cfeb9
Len <len.zeecode@gmail.com>
2019-02-02 23:31:16 +0700

    update contact page

--- wp-content/themes/academy/page-template/contact_page.php
+++ wp-content/themes/academy/page-template/contact_page.php
@@ -1,113 +1,130 @@
-<?php 
-	// Template name: Contact Page
+<?php
+	// Template name: Contact page
 
-	if (isset($_POST['submit'])) {
-	
-		ini_set( 'display_errors', 1 );
-    	error_reporting( E_ALL );
-		
-		$name = !empty($_POST['username']) ? $_POST['username'] : '';
-		$email = !empty($_POST['email']) ? $_POST['email'] : '';
-		$website = isset($_POST['website']) ? isset($_POST['website']) : '';
-		$comment = !empty($_POST['message']) ? $_POST['message'] : '';
-
-		$headers =  "MIME-Version: 1.0" . "\r\n"; 
-		$headers .= "From: $name $email " . "\r\n";
-		$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n"; 
-		$to = 'len.zeecode@gmail.com';
-		$subject = 'International Business Institute';
-		$message = "Dear IBI " . "<br />";
-		$message .= $comment;
-
-		$mail = mail( $to, $subject, $message, $headers );
-
-		if ($email) {
-			echo "Sent";
-		} else {
-			echo "Not send";
-		}
-	}
-	
 	get_header();
+
+	$args = array(
+		'post_type' => 'page',
+		'pagename' => 'contact-us',
+		'posts_per_page' => 1,
+		'post_status' => 'publish'
+	);
+	$query = new WP_Query($args);
 ?>
 
 
 <?php include(TEMPLATEPATH . '/page-template/breadcrumb.php'); ?>
 
 <!-- Contact Us -->
-		<section id="contact" class="contact section">
-			<div class="container">
+	<section id="contact" class="contact section">
+		<div class="container">
+			<?php 
+				if($query->have_posts()) : 
+					while($query->have_posts()) : $query->the_post(); 
+			?>
+			
 				<div class="row">
 					<div class="col-md-12">
 						<div class="section-title">
 							<h2><span>Contact</span> Information</h2>
-							<?php if(!empty(otherSections('footer_description'))) : ?>
-								<p><?php otherSections('footer_description'); ?></p>
-							<?php endif; ?>
+						<?php the_field('contact_description'); ?>
 						</div>
 					</div>
 				</div>
 				<div class="contact-head">
 					<div class="row">
-						<div class="col-lg-8 col-md-8 col-12">
-							<div class="form-head">
-								<!-- Form -->
-								<form class="form" method="post">
-									<div class="form-group">
-										<input name="username" type="text" placeholder="Enter Name *" autocomplete="off">
-									</div>
-									<div class="form-group">
-										<input name="email" type="email" placeholder="Email Address *" autocomplete="off">
-									</div>
-									<div class="form-group">
-										<input name="subject" type="text" placeholder="Website" autocomplete="off">
-									</div>
-									<div class="form-group">
-										<textarea name="message" placeholder="Comment *"></textarea>
-									</div>
-									<div class="form-group">
-										<div class="button">
-											<button type="submit" name="submit" class="btn primary">Post Comment</button>
+						<div class="col-lg-6 col-md-6 col-12">
+							<div class="contact-map">
+								<!-- Map -->
+								<div id="map">
+									<?php the_field('map_location'); ?>
+								</div>
+								<!--/ End Map -->
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-12">
+							<div class="contact_infor">
+								<ul>
+									<li><label><i class="fa fa-map-marker"></i>Address</label>
+										<div class="des"><?php the_field('address'); ?></div>
+									</li>
+									<li><label><i class="fa fa-phone"></i>Phone Number</label>
+										<div class="des">
+											<?php 
+												$i = 0;
+												if(have_rows('phone_number')) :
+													while (have_rows('phone_number')) : the_row();
+														$i++;
+												?>
+												<?php 
+													if($i == 1) echo '';
+													else echo '|';
+												?>
+												<a href="tel:<?php the_sub_field('phone_number'); ?>">
+													<?php the_sub_field('phone_number'); ?>
+												</a> 
+												<?php
+													endwhile;
+												endif;
+											?>
 										</div>
-									</div>
-								</form>
-								<!--/ End Form -->
-							</div>
-						</div>
+									</li>
+									<li><label><i class="fa fa-print"></i>Fax no</label>
+										<div class="des">
+											<?php 
+												$i = 0;
+												if(have_rows('fax_numbers')) :
+													while (have_rows('fax_numbers')) : the_row();
+														$i++;
+												?>
+												<?php 
+													if($i == 1) echo '';
+													else echo '|';
+												?>
+												<?php the_sub_field('fax'); ?>
 
-						<?php get_sidebar(); ?>
+												<?php
+													endwhile;
+												endif;
+											?>
+										</div>
+									</li>
+									<li><label><i class="fa fa-envelope"></i>Email Address</label>
+										<div class="des">
+											<?php 
+												$i = 0;
+												if(have_rows('emails')) :
+													while (have_rows('emails')) : the_row();
+														$i++;
+												?>
+												<?php 
+													if($i == 1) echo '';
+													else echo '|';
+												?>
+												<a href="mailto:<?php the_sub_field('email'); ?>">
+													<?php the_sub_field('email'); ?>
+												</a>
+												<?php
+													endwhile;
+												endif;
+											?>
+										</div>
+									</li>
+									<li>
+										<label><i class="fa fa-clock-o"></i>WORKING HOURS</label>
+										<div class="des">
+											<?php the_field('working_hour'); ?>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="contact-bottom">
-					<div class="row">
-						<div class="col-lg-4 col-md-4 col-12">
-							<!-- Contact-Info -->
-							<div class="contact-info">
-								<div class="icon"><i class="fa fa-map"></i></div>
-								<h3>Location</h3>
-								<p><?php otherSections('footer_address'); ?></p>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-12">
-							<!-- Contact-Info -->
-							<div class="contact-info">
-								<div class="icon"><i class="fa fa-envelope"></i></div>
-								<h3>Email Address</h3>
-								<?php otherSections('footer_email'); ?>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-4 col-12">
-							<!-- Contact-Info -->
-							<div class="contact-info">
-								<div class="icon"><i class="fa fa-phone"></i></div>
-								<h3>Get in Touch</h3>
-								<p><?php otherSections('footer_phone'); ?></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!--/ End Contact Us -->
+
+			<?php endwhile; endif; ?>
+		</div>
+	</section>
+	<!--/ End Contact Us -->
 
 <?php get_footer(); ?>

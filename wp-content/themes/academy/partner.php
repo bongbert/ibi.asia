38fb4bbfe2610e55ad07be4c1d674fde2027a44d
Len <len.zeecode@gmail.com>
2019-02-05 22:29:02 +0700

    update partnership

---
+++ wp-content/themes/academy/partner.php
@@ -0,0 +1,38 @@
+<?php 
+	$args = array(
+		'post_type' => 'partnership',
+		'post_status' => 'publish',
+		'posts_per_page' => 3
+	);
+	$query = new WP_Query($args);
+
+	if ($query->have_posts()) : 
+?>
+<section class="container">
+	
+	<div class="row">
+		<div class="col-12 wow zoomIn">
+			<div class="section-title">
+				<h3><?php otherSections('partner_title'); ?></h3>
+				<p><?php otherSections('partner_description'); ?></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<div class="owl-carousel owl-theme">
+				<?php
+					while($query->have_posts()) : $query->the_post();
+						$image = get_field('icon');
+				 ?>
+				    <div class="item">
+				     	<img src="<?= $image['url']; ?>" class="img-responsive image-partner" alt="<?php the_title(); ?>" title="<?php the_title(); ?>">
+				    </div>
+				<?php endwhile; ?>
+			</div>
+		  
+		</div>
+	</div>
+</section>
+<?php endif; ?>

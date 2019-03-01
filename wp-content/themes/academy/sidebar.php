d10f9e0e8e1a800721da3507f7c5faec78ec349f
Len <len.zeecode@gmail.com>
2019-03-01 23:27:02 +0700

    update sidebar

--- wp-content/themes/academy/sidebar.php
+++ wp-content/themes/academy/sidebar.php
@@ -1,18 +1,5 @@
 <div class="col-lg-4 col-12">
 	<div class="learnedu-sidebar">
-		<!-- Categories -->
-		<div class="single-widget categories">
-			 <h3 class="title">Categories</h3>
-			 <ul>
-				<?php 
-					$categories = get_categories();
-					foreach($categories as $category) {
-					   echo '<li><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
-					}
-				?>
-			</ul>
-		</div>
-		<!--/ End Categories -->
 		
 		<?php include(TEMPLATEPATH . '/page-template/recent_post.php'); ?>
 	</div>
